48ade9a3fd656784aad313a8cc7bd84ebd2771fd
DavidManiIbrahim <ibrahimmanidavid@gmail.com>
2024-11-26 16:13:17 +0100

    arranged connect btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,8 +24,8 @@ const Navbar = () => {
                 <Link to="/features">Features</Link>
                 <Link to="/beneficiaries">Beneficiaries</Link>
                 <Link to="/donors">Donors</Link>
-            <button className={styles.contactButton} >Connect Wallet</button>
             </nav>
+            <button className={styles.contactButton} >Connect Wallet</button>
             
             <div className={`${styles.menuIcon} ${isOpen ? styles.open : ''}`} onClick={toggleMenu}>
                 <div></div>
